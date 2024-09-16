a6557afbba2ea9b61f54a5cb7f1e52b2256c0946
Nishant Kumar <nk7970922655@gmail.com>
2024-09-16 22:51:28 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+const Hero=()=>{
+    return(<main className="hero">
+        <div className="content">
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+            <p>YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP YOU WIYH OUR SHOES.YOUR FEET DESREVE THE BEST AND WE ARE HERE TO HELP YOU WITJ OUR SHOES</p>
+            <div className="hero-btn">
+                <button>ShopNow</button>
+                <button className="secondary-btn">Category</button>
+            </div>
+            <div className="p"><p>Also Availabe on</p>
+            <div className="brand-icons">
+                <img src="/images/flipkart.png" alt="flipkart-logo" width={50} />
+                <img src="/images/amazon.png" alt="amazon-logo"  width={50}/>
+            </div>
+            
+            </div>
+        </div>
+        <div className="image">
+            <img src="/images/hero_image.png" alt="shoe" width={600}/>
+        </div>
+
+    </main>)
+}
+export default Hero;
---
+++ src/components/navigation.jsx
@@ -0,0 +1,18 @@
+const Navigation=()=>{
+    return( <nav>
+        <div className="logo">
+            <img src="../public/images/d5389cc06b9d40c646878c60ac4bf7ed.png" alt="logo" width={60}/>
+        </div>
+        
+    <ul>
+        <li><a href="#">Menu</a></li>
+        <li><a href="#">Location</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+
+
+        <button>Login</button>
+    </nav>)
+}
+export default Navigation;
